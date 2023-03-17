8f7c21303daba981a9ee9d1ac280c8194d0109cb
Jake Boone <jakeboone02@gmail.com>
2023-03-17 12:24:59 -0700

    Clarify pass/fail cases

--- src/testrun.ts
+++ src/testrun.ts
@@ -1,8 +1,22 @@
 import { produce } from "immer";
 
-export const runTest = (t: any, e: any) => {
-  t("string[]", () => {
-    const newObj = produce(['object in array'], (draft) => draft);
-    e(newObj).toEqual(['object in array']);
+const immerTheArray = (array: any[]) => produce(array, (draft) => draft);
+
+export const runTest = (test: any, expect: any) => {
+  test("FAILS IN BUN: compare to equivalent non-empty array", () => {
+    const array1 = [""];
+    const array2 = [""];
+    expect(immerTheArray(array1)).toEqual(array2);
+  });
+
+  test("PASSES IN BOTH: compare to itself", () => {
+    const array1 = [""];
+    expect(immerTheArray(array1)).toEqual(array1);
+  });
+
+  test("PASSES IN BOTH: compare to equivalent empty array", () => {
+    const array1: string[] = [];
+    const array2: string[] = [];
+    expect(immerTheArray(array1)).toEqual(array2);
   });
 };
